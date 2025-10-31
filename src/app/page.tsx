794124128c7bd3f273d7da2031c24e0df87364ae
Семён Субочев <s.subochev@sz.ld>
2025-10-31 16:59:44 +0300

    productCard и Акции

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Actions from './components/Actions';
+import ProductCard from './components/ProductCard';
 import Slider from './components/Slider/Slider';
 
 export default function Home() {
 	return (
 		<main className="w-full mx-auto mb-20">
 			<Slider />
+			<div className="px-[max(12px,calc((100%-1208px)/2))] flex flex-col gap-y-20 md:mb-25 xl:mb-30">
+				<Actions />
+			</div>
 		</main>
 	);
 }
