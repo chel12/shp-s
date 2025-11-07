eb5b820587b5b1538ddd00924e8d7e6ab32e4f25
Семён Субочев <s.subochev@sz.ld>
2025-11-07 14:46:22 +0300

    img for catalog + apiToBD + page initial

--- src/components/header/ButtonSearch.tsx
+++ src/components/header/ButtonSearch.tsx
@@ -1,18 +1,23 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const ButtonSearch = () => {
-  return (
-    <button className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
-      <Image
-        src="/icons-header/icon-menu.svg"
-        alt="menu"
-        width={24}
-        height={24}
-        className="hidden md:block"
-      />
-      <span className="text-base text-white hidden lg:block">Каталог</span>
-    </button>
-  );
+	return (
+		<Link
+			href={'/catalog'}
+			className="bg-(--color-primary) hover:shadow-(--shadow-button-default) active:shadow-(--shadow-button-active) hidden md:flex w-10 p-2 gap-4 lg:w-35 cursor-pointer duration-300 rounded">
+			<Image
+				src="/icons-header/icon-menu.svg"
+				alt="menu"
+				width={24}
+				height={24}
+				className="hidden md:block"
+			/>
+			<span className="text-base text-white hidden lg:block">
+				Каталог
+			</span>
+		</Link>
+	);
 };
 
 export default ButtonSearch;
