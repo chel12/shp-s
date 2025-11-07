4383960431c5dfc69308f01549fa83661a2525ee
Семён Субочев <s.subochev@sz.ld>
2025-11-07 16:51:46 +0300

    вынос логики грида в блок

---
+++ src/app/(catalog)/GridCategoryBlock.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const GridCategoryBlock = ({
+	id,
+	title,
+	img,
+}: {
+	id: number;
+	title: string;
+	img: string;
+}) => {
+	return (
+		<Link
+			href={`category-${id}`}
+			className="block relative h-full overflow-hidden group min-w-40
+					         md:min-w-[224px] xl:min-w[274px]">
+			<Image
+				src={img}
+				alt={title}
+				fill
+				sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw"
+				className="object-cover transition-transform group-hover:scale-105"
+			/>
+			<div
+				className="absolute inset-0 bg-[linear-gradient(180deg,rgba(112,192,91,0)_0%,rgba(112,192,91,0.82)_82.813%)] h-[117px] top-auto
+                  group-hover:bg-[linear-gradient(180deg,rgba(255,102,51,0)_0%,rgba(255,102,51,1)_100%)] group-hover:h-[177px] transition-all duration-300s"></div>
+			<div className="absolute left-2.5 bottom-2.5 flex items-center ">
+				<span className="text-white text-lg font-bold">{title}</span>
+			</div>
+		</Link>
+	);
+};
+
+export default GridCategoryBlock;
--- src/app/(catalog)/catalog/page.tsx
+++ src/app/(catalog)/catalog/page.tsx
@@ -1,9 +1,8 @@
 'use client';
 
 import { CatalogProps } from '@/types/catalog';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import GridCategoryBlock from '../GridCategoryBlock';
 
 const CatalogPage = () => {
 	const [categories, setCategories] = useState<CatalogProps[]>([]);
@@ -48,7 +47,9 @@ const CatalogPage = () => {
 
 	return (
 		<section className="px-[max(12px,calc((100%-1208px)/2))] mx-auto mb-20">
-			<h1 className="mb-4 md:mb-8 xl:mb-10 flex flex-row text-4xl mb:text-5xl xl:text-[64px] text-[#414141]"></h1>
+			<h1 className="mb-4 md:mb-8 xl:mb-10 flex flex-row text-4xl mb:text-5xl xl:text-[64px] text-[#414141] font-bold">
+				Каталог
+			</h1>
 			<div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
 				{categories.map((category) => (
 					<div
@@ -59,26 +60,11 @@ const CatalogPage = () => {
 						${category.colSpan}
 						bg-gray-100 rounded overflow-hidden min-h-50`}>
 						<div className="h-full w-full">
-							<Link
-								href={`category-${category.id}`}
-								className="block relative h-full overflow-hidden group min-w-40
-					         md:min-w-[224px] xl:min-w[274px]">
-								<Image
-									src={category.img}
-									alt={category.title}
-									fill
-									sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw,33vw"
-									className="object-cover transition-transform group-hover:scale-105"
-								/>
-								<div
-									className="absolute inset-0 bg-[linear-gradient(180deg,rgba(112,192,91,0)_0%,rgba(112,192,91,0.82)_82.813%)] h-[117px] top-auto
-                  group-hover:bg-[linear-gradient(180deg,rgba(255,102,51,0)_0%,rgba(255,102,51,1)_100%)] group-hover:h-[177px] transition-all duration-300s"></div>
-								<div className="absolute left-2.5 bottom-2.5 flex items-center ju">
-									<span className="text-white text-lg font-bold">
-										{category.title}
-									</span>
-								</div>
-							</Link>
+							<GridCategoryBlock
+								id={category.id}
+								title={category.title}
+								img={category.img}
+							/>
 						</div>
 					</div>
 				))}
