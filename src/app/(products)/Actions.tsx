1e0f01c54b94be45148ed376accdeb7fa5606ae4
Семён Субочев <s.subochev@sz.ld>
2025-11-08 10:11:37 +0300

    перемешка с сервера, функция урла изменена

--- src/app/(products)/Actions.tsx
+++ src/app/(products)/Actions.tsx
@@ -1,25 +1,27 @@
-import fetchProductsByCategory from "./fetchProducts";
-import ProductsSection from "../../components/ProductsSection";
-import { shuffleArray } from "../../../utils/shuffleArray";
+import fetchProductsByCategory from './fetchProducts';
+import ProductsSection from '../../components/ProductsSection';
+import { CONFIG } from '../../../config/config';
 
 const Actions = async () => {
-  try {
-    let products = await fetchProductsByCategory("actions");
-    products = shuffleArray(products);
+	try {
+		const { items } = await fetchProductsByCategory('actions', {
+			randomLimit: CONFIG.ITEMS_PER_PAGE_MAIN_PRODUCTS,
+		});
 
-    return (
-      <ProductsSection
-        title="Акции"
-        viewAllButton={{ text: "Все акции", href: "actions" }}
-        products={products}
-        compact
-      />
-    );
-  } catch {
-    return (
-      <div className="text-red-500">Ошибка: не удалось загрузить акции</div>
-    );
-  }
+		return (
+			<ProductsSection
+				title="Акции"
+				viewAllButton={{ text: 'Все акции', href: 'actions' }}
+				products={items}
+			/>
+		);
+	} catch {
+		return (
+			<div className="text-red-500">
+				Ошибка: не удалось загрузить акции
+			</div>
+		);
+	}
 };
 
 export default Actions;
