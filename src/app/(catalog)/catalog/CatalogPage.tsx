6d4028802e3efe7479bc7bffebc900712705d2f8
Семён Субочев <s.subochev@sz.ld>
2025-11-25 18:37:51 +0300

    rafctoring/рефактор сетки грида для драгн дропа в админке

--- src/app/(catalog)/catalog/CatalogPage.tsx
+++ src/app/(catalog)/catalog/CatalogPage.tsx
@@ -1,11 +1,10 @@
 'use client';
-
 import { CatalogProps } from '@/types/catalog';
 import { useEffect, useState } from 'react';
-import GridCategoryBlock from '../GridCategoryBlock';
 import ErrorComponent from '@/components/ErrorComponent';
 import { Loader } from '@/components/Loader';
 import CatalogAdminControls from '../CatalogAdminControls';
+import CatalogGrid from '../CatalogGrid';
 
 export const metadata = {
 	title: 'Каталог товаров магазина "Северяночка"',
@@ -206,42 +205,16 @@ const CatalogPage = () => {
 			<h1 className="mb-4 md:mb-8 xl:mb-10 flex flex-row text-4xl mb:text-5xl xl:text-[64px] text-[#414141] font-bold">
 				Каталог
 			</h1>
-			<div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
-				{categories.map((category) => (
-					<div
-						key={category._id}
-						className={`${category.mobileColSpan} ${
-							category.tabletColSpan
-						} ${
-							category.colSpan
-						} bg-gray-100 rounded overflow-hidden min-h-50 h-full
-            ${isEditing ? 'border-4 border-dashed border-gray-400' : ''}
-            ${
-				hoveredCategoryId === category._id
-					? 'border-3 border-red-800'
-					: ''
-			}
-                `}
-						onDragOver={(e) => handleDragOver(e, category._id)}
-						onDragLeave={handleDragLeave}
-						onDrop={(e) => handleDrop(e, category._id)}>
-						<div
-							className={`h-full w-full ${
-								draggedCategory?._id === category._id
-									? 'opacity-50'
-									: ' '
-							}`}
-							draggable={isEditing}
-							onDragStart={() => handleDragStart(category)}>
-							<GridCategoryBlock
-								id={category.id}
-								title={category.title}
-								img={category.img}
-							/>
-						</div>
-					</div>
-				))}
-			</div>
+			<CatalogGrid
+				categories={categories}
+				isEditing={isEditing}
+				draggedCategory={draggedCategory}
+				hoveredCategoryId={hoveredCategoryId}
+				onDragStartAction={handleDragStart}
+				onDragOverAction={handleDragOver}
+				onDragLeaveAction={handleDragLeave}
+				onDropAction={handleDrop}
+			/>
 		</section>
 	);
 };
