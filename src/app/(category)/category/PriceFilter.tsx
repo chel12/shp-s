cebf537d9f36a2ca0dc1d6641ff1973ec162d54a
Семён Субочев <s.subochev@sz.ld>
2025-11-30 13:56:57 +0300

    функция отправки applyPriceFilter

--- src/app/(category)/category/PriceFilter.tsx
+++ src/app/(category)/category/PriceFilter.tsx
@@ -1,14 +1,12 @@
 'use client';
 import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
-import { useCallback, useState } from 'react';
-
-export interface PriceFilterProps {
-	basePath: string;
-	category: string;
-}
+import { useSearchParams, useRouter } from 'next/navigation';
+import { FormEvent, useCallback, useState } from 'react';
+import { CONFIG } from '../../../../config/config';
+import { PriceFilterProps, PriceRange } from '@/types/priceTypes';
 
 const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
+	const router = useRouter();
 	const searchParams = useSearchParams();
 	//из урла берём
 	const urlPriceFrom = searchParams.get('priceFrom') || '';
@@ -18,6 +16,32 @@ const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
 		from: urlPriceFrom,
 		to: urlPriceTo,
 	});
+	const [priceRange, setPriceRange] = useState<PriceRange>(
+		CONFIG.FALLBACK_PRICE_RANGE
+	);
+
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		applyPriceFilter();
+	};
+
+	const applyPriceFilter = useCallback(() => {
+		const params = new URLSearchParams(searchParams.toString());
+		//защита от пользователя чтобы меньше положенного не ввёл
+		let fromValue = Math.max(
+			priceRange.min,
+			parseInt(inputValues.from) || priceRange.min
+		);
+		//защита от пользователя чтобы больше положенного не ввёл
+		let toValue = Math.min(
+			priceRange.max,
+			parseInt(inputValues.to) || priceRange.max
+		);
+		if (fromValue > toValue) [fromValue, toValue] = [toValue, fromValue];
+		params.set('priceFrom', fromValue.toString());
+		params.set('priceTo', toValue.toString());
+		router.push(`${basePath}?${params.toString()}`);
+	}, [inputValues, priceRange, searchParams, router, basePath]);
 
 	const handleInputChange = useCallback(
 		(e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,10 +52,15 @@ const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
 	);
 
 	return (
-		<div className="flex flex-col gap-y-10 text-[#414141] mt-10 xl:mt-0">
+		<form
+			onSubmit={handleSubmit}
+			className="flex flex-col gap-y-10 text-[#414141] mt-10 xl:mt-0">
 			<div className="flex flex-row justify-between items-center">
 				<p className="text-black text-base">Цена</p>
-				<button className="text-xs rounded bg-[#f3f2f1] h-8 p-2 cursor-pointer">
+				<button
+					type="button"
+					onClick={resetPriceFilter}
+					className="text-xs rounded bg-[#f3f2f1] h-8 p-2 cursor-pointer">
 					Очистить
 				</button>
 			</div>
@@ -39,6 +68,7 @@ const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
 				<input
 					type="number"
 					name="from"
+					placeholder={`${priceRange.min}`}
 					value={inputValues.from}
 					onChange={handleInputChange}
 					min={priceRange.min}
@@ -54,6 +84,7 @@ const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
 				<input
 					type="number"
 					name="to"
+					placeholder={`${priceRange.max}`}
 					value={inputValues.to}
 					onChange={handleInputChange}
 					min={priceRange.min}
@@ -61,7 +92,13 @@ const PriceFilter = ({ basePath, category }: PriceFilterProps) => {
 					className="w-[124px] h-10 border border-[#bfbfbf] rounded bg-white py-2 px-4"
 				/>
 			</div>
-		</div>
+			<button
+				type="submit"
+				className="bg-[#ff6633] text-white hover:shadow-(--shadow-article)
+			active:shadow-(--shadow-button-active) h-10 rounded justify-center
+			items-center duration-300 cursor-pointer
+			"></button>
+		</form>
 	);
 };
 
--- src/types/priceTypes.ts
+++ src/types/priceTypes.ts
@@ -2,3 +2,7 @@ export interface PriceFilterProps {
 	basePath: string;
 	category: string;
 }
+export type PriceRange = {
+	min: number;
+	max: number;
+};
