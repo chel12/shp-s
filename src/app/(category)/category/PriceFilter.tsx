834c850f2f3b900df9886774f09ea9161d992d3a
Семён Субочев <s.subochev@sz.ld>
2025-12-01 10:35:08 +0300

    refactoring чекбокса

--- src/app/(category)/category/PriceFilter.tsx
+++ src/app/(category)/category/PriceFilter.tsx
@@ -9,6 +9,7 @@ import MiniLoader from '@/components/MiniLoader';
 import PriceFilterHeader from './PriceFilterHeader';
 import PriceInputs from './PriceInputs';
 import PriceRangeSlider from './PriceRangeSlider';
+import InStockToggle from './inStockToggle';
 
 const PriceFilter = ({
 	basePath,
@@ -113,14 +114,6 @@ const PriceFilter = ({
 		router.push(`${basePath}?${params.toString()}`);
 	}, [inputValues, priceRange, searchParams, router, basePath, inStock]);
 
-	//функция "в наличии"
-	const handleInStockChange = useCallback(
-		(e: React.ChangeEvent<HTMLInputElement>) => {
-			setInStock(e.target.checked);
-		},
-		[]
-	);
-
 	//стейт для rc слайдера
 	const sliderValues = [
 		parseInt(inputValues.from) || priceRange.min,
@@ -184,37 +177,10 @@ const PriceFilter = ({
 				onChangeAction={handleSliderChange}
 			/>
 
-			<div className="flex items-center gap-2">
-				<label className="relative inline-flex items-center cursor-pointer">
-					<input
-						type="checkbox"
-						id="inStock"
-						checked={inStock}
-						onChange={handleInStockChange}
-						className="sr-only peer"
-					/>
-					<div className="w-[46px] h-6 bg-gray-200 rounded-full peer peer-checked:bg-[#70c05b] transition-colors duration-200">
-						<div
-							className={`
-                absolute top-0.5 left-0
-                w-5 h-5
-                border-[0.5px] border-[rgba(0,0,0,0.04)]
-                rounded-full
-                shadow-[0px_1px_1px_rgba(0,0,0,0.08),0px_2px_6px_rgba(0,0,0,0.15)]
-                bg-white
-                transition-transform duration-300
-                ${
-					inStock
-						? 'transform translate-x-6'
-						: 'transform translate-x-0'
-				}
-              `}></div>
-					</div>
-					<span className="ml-2 text-sm text-[#414141]">
-						В наличии
-					</span>
-				</label>
-			</div>
+			<InStockToggle
+				checked={inStock}
+				onChangeAction={(checked) => setInStock(checked)}
+			/>
 			<button
 				type="submit"
 				className="bg-[#ff6633] text-white hover:shadow-(--shadow-article)
