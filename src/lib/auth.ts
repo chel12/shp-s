243d2fb3d93f239f4b9e0b81e64d0b5b3717afb5
Семён Субочев <s.subochev@sz.ld>
2025-12-06 18:35:15 +0300

    бек:отправка + ui компонент письма

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,47 @@
+
+import VerifyEmail from '@/app/(auth)/(reg)/_components/VerifyEmail';
 import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { MongoClient } from 'mongodb';
+import { Resend } from 'resend';
 
 const client = new MongoClient(process.env.DELIVERY_SHOP_DB_URL!);
 const db = client.db('delivery-shop');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
 	database: mongodbAdapter(db),
-	//...
+	emailAndPassword: {
+		enabled: true,
+		requireEmailVerification: true,
+	},
+	emailVerification: {
+		sendVerificationEmail: async ({ user, url }) => {
+			await resend.emails.send({
+				//почтовый домен надо/заглушка щас
+				from: 'Северяночка <onboarding@resend.dev>',
+				//куда отправлять
+				to: user.email,
+				//заголовок
+				subject: 'Подтвердите email',
+				//компонент - тело письма
+				react: VerifyEmail({ username: user.name, verifyUrl: url }),
+			});
+		},
+		expiresIn: 86400,
+		autoSignInAfterVerification: false,
+	},
+	user: {
+		additionalFields: {
+			//имя и емаил не надо, это система будет автоматом писать
+			phoneNumber: { type: 'string', input: true, required: true },
+			surname: { type: 'string', input: true, required: true },
+			birthdayDate: { type: 'date', input: true, required: true },
+			region: { type: 'string', input: true, required: true },
+			location: { type: 'string', input: true, required: true },
+			gender: { type: 'string', input: true, required: true },
+			card: { type: 'string', input: true, required: false },
+			hasCard: { type: 'boolean', input: true, required: false },
+		},
+	},
 });
